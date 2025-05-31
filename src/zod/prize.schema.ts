72e50a514a55ce57396a1fe7d31112a081a7fea9
Mr M <github.com.nativity328@passmail.net>
2025-05-30 22:23:14 -0300

    adiciona input de moeda

--- src/zod/prize.schema.ts
+++ src/zod/prize.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const prizeSchema = z.object({
   name: z.string().min(1, 'Nome é obrigatório'),
   description: z.string().min(1, 'Descrição é obrigatória'),
-  value: z.string().min(1, 'Valor é obrigatório'),
+  value: z.coerce.number().min(1, 'Valor é obrigatório'),
 
 });
 
